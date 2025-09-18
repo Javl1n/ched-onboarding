434cce8eff26f62b0412c4df355283ac35fd3eaa
Javl1n <farmodia@gmail.com>
2025-09-19 02:33:18 +0800

    optimize video input

--- resources/js/components/onboarding/create/video-input.tsx
+++ resources/js/components/onboarding/create/video-input.tsx
@@ -2,23 +2,36 @@ import { InputHTMLAttributes, useEffect, useRef, useState } from "react";
 import BlockLayout, { BlockAttributes, OnboardingInputAttributes } from "./block-layout";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { LoaderCircle } from "lucide-react";
+import { motion, useTime, useTransform } from "motion/react"
 
 export default function VideoInput({isNew, ...props}: InputHTMLAttributes<HTMLInputElement> & BlockAttributes) {
      const inputRef = useRef<HTMLInputElement | null>(null);
      const [previewUrl, setPreviewUrl] = useState<string | null>(props.value as string);
+     const [isLoading, setIsLoading] = useState<boolean>(false);
+     const time = useTime();
+     const rotate = useTransform(time, [0, 1000], [0, 360], { clamp: false });
 
      useEffect(() => {
           const input = inputRef.current;
+          let timeoutId: NodeJS.Timeout;
 
           if (!input) return;
      
           const handleInputChange = () => {
-               setPreviewUrl(input.value.replace("watch?v=", "embed/").replace("view?usp=drive_link", "preview"));
+               clearTimeout(timeoutId);
+               setPreviewUrl(null);
+               setIsLoading(true);
+               timeoutId = setTimeout(() => {
+                    setPreviewUrl(input.value.replace("watch?v=", "embed/").replace("view?usp=drive_link", "preview"));
+                    setIsLoading(false);
+               }, 500);
           }
      
           input.addEventListener('input', handleInputChange);
      
           return () => {
+               clearTimeout(timeoutId);
                input.removeEventListener('input', handleInputChange);
           }
      }, []);
@@ -34,6 +47,13 @@ export default function VideoInput({isNew, ...props}: InputHTMLAttributes<HTMLIn
                          src={`${previewUrl}`}>
                     </iframe> 
                : null}
+               {isLoading ?
+                    <div className="w-full aspect-video border rounded-lg flex justify-center">
+                         <motion.div className="my-auto" style={{rotate}}>
+                              <LoaderCircle  />
+                         </motion.div>
+                    </div>
+               : null }
           </div>
      )
 }
